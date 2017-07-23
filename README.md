3ca0f63c01fbdbc210f743e5785ada86a8025b76
antoine.derengerve <antoine.derengerve@partnering.fr>
2017-07-24 00:50:46 +0200

    Creation

--- README.md
+++
@@ -1,2 +0,0 @@
-# encoding
-String based communication encoding
---
+++ encoding.js
@@ -0,0 +1,252 @@
+/* maya-client
+ * Copyright (c) 2014, Partnering Robotics, All rights reserved.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version
+ *	3.0 of the License. This library is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+/**
+ * Management of channel encoding
+ * - base64 coding
+ * - none
+ * Data format :
+ *		t: {'b64','none'}
+ *		b: <if b64> {4,8}
+ *		d: encoded data {buffer or Array}
+ *		s: size
+ */
+
+
+var base64 = require('base-64');
+
+/**
+ * Default : no encoding
+ * */
+function NoCoding(){
+	return this;
+};
+
+/**
+*
+*/
+NoCoding.prototype.from = function(data) {
+	if(data.d === 'number' || Array.isArray(data.d))
+		return data.d;
+	else
+		return data;
+};
+
+/**
+*/
+NoCoding.prototype.to = function(array) {
+	return {
+		t: 'no', /* type */
+		d: array, /* data */
+		s: array.length
+	};
+};
+
+
+
+
+/**
+ * Management of base64 encoding
+ * Effective for string based channels (like JSON based WS)
+ * */
+function Base64Coding(){
+	return this;
+};
+
+
+
+////////////////////////////////////////////////////////////////
+/////////////////    Utility functions    //////////////////////
+////////////////////////////////////////////////////////////////
+
+/*\
+ |*|
+ |*|  utilitaires de manipulations de chaînes base 64 / binaires / UTF-8
+ |*|
+ |*|  https://developer.mozilla.org/fr/docs/Décoder_encoder_en_base64
+ |*|
+ \*/
+/** Decoder un tableau d'octets depuis une chaîne en base64 */
+var b64ToUint6 = function(nChr) {
+	return nChr > 64 && nChr < 91 ?
+		nChr - 65
+		: nChr > 96 && nChr < 123 ?
+		nChr - 71
+		: nChr > 47 && nChr < 58 ?
+		nChr + 4
+		: nChr === 43 ?
+		62
+		: nChr === 47 ?
+		63
+		:	0;
+};
+
+/**
+ * Decode base64 string to UInt8Array
+ * @param  {String} sBase64		base64 coded string
+ * @param  {int} nBlocksSize size of blocks of bytes to be read. Output byteArray length will be a multiple of this value.
+ * @return {Uint8Array}				tab of decoded bytes
+ */
+var base64DecToArr = function(sBase64, nBlocksSize) {
+	var
+	sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
+	nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2,
+	buffer = new ArrayBuffer(nOutLen), taBytes = new Uint8Array(buffer);
+
+	for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+		nMod4 = nInIdx & 3; /* n mod 4 */
+		nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+		if (nMod4 === 3 || nInLen - nInIdx === 1) {
+			for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+				taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+			}
+			nUint24 = 0;
+		}
+	}
+	// console.log("u8int : "+JSON.stringify(taBytes));
+	return buffer;
+};
+
+////////////////////////////////////////////////////////////////
+/////////////////   Interface functions   //////////////////////
+////////////////////////////////////////////////////////////////
+
+
+/**
+* Convert buffer coded in base64 and containing numbers coded by
+* byteCoding bytes into array
+* @param buffer in base64
+* @param byteCoding number of bytes for each number (4 or 8)
+* @return array of float (32 or 64). null if could not convert.
+*/
+Base64Coding.prototype.from = function(data) {
+	var byteCoding = data.b;
+
+	/* check byte coding */
+	if(byteCoding !== 4 && byteCoding !== 8) {
+		return null;
+	}
+
+	/* decode data to array of byte */
+	var buf = base64DecToArr(data.d, data.b);
+	/* parse data to float array */
+	var fArray=null;
+	switch(data.b) {
+	case 4:
+		fArray = new Float32Array(buf);
+		break;
+	case 8:
+		fArray = new Float64Array(buf);
+		break;
+	default:
+		console.log("Unexpected byteCoding! Should not happen!!");
+		return null;
+	}
+	/* parse fArray into normal array */
+	var tab = [].slice.call(fArray);
+
+	if(data.s !== tab.length) {
+		console.log("Size mismatch when decoding !");
+		return null;
+	}
+	return tab;
+};
+
+/**
+* Convert array containing numbers coded by byteCoding bytes into buffer coded in base64
+* @param 	{Array<Float>} 	array of float (32 or 64 bits)
+* @param 	{integer} 	byteCoding number of bytes for each float (4 or 8)
+* @return  	{String} 	buffer in base64. null if could not convert.
+*/
+Base64Coding.prototype.to = function(array, byteCoding) {
+	/* check byte coding */
+	if(byteCoding !== 4 && byteCoding !== 8) {
+		return null;
+	}
+
+	/*** case ArrayBuffer ***/
+	var buffer = new ArrayBuffer(array.length*byteCoding);
+	switch(byteCoding) {
+	case 4:
+		var buf32 = new Float32Array(buffer);
+		buf32.set(array);
+		break;
+	case 8:
+		var buf64 = new Float64Array(buffer);
+		buf64.set(array);
+		break;
+	}
+	var buffChar = new Uint8Array(buffer);
+	var buffCharCoded = new Array(buffChar.length);
+	for(var n =0; n<buffChar.length; n++) {
+		buffCharCoded[n] = String.fromCharCode(buffChar[n]);
+	}
+	var str = new String(buffCharCoded.join(''));
+	var b64Buff = base64.encode(str);
+	return {
+		t: 'b64', /* type */
+		b: byteCoding, /* byteCoding */
+		d: b64Buff, /* data */
+		s: array.length /* size */
+	};
+};
+
+
+
+
+/**
+ * Management of comm encoding
+ * */
+function CodingHandler(){
+	this.b64 = new Base64Coding();
+	this.none = new NoCoding();
+
+	return this;
+};
+
+
+CodingHandler.prototype.from = function(data) {
+	if(typeof data == 'undefined' || data==null)
+		return null;
+	switch(data.t) {
+	case 'b64':
+		return this.b64.from(data);
+	default:
+		return this.none.from(data);
+	}
+};
+
+
+CodingHandler.prototype.to = function(array, type, byteCoding) {
+	if(typeof array === 'number') {
+		array=[array];
+	}
+	if(!Array.isArray(array)){
+		console.log("CodingHandler.to only accepts array !");
+		return null;
+	}
+
+	switch(type) {
+	case 'b64':
+		return this.b64.to(array, byteCoding);
+	case 'no':
+	default:
+		return this.none.to(array);
+	}
+};
+
+var encode = new CodingHangler();
+
+module.exports = encode;
+
+
